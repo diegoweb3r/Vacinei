983800ba3a096f35bb094316edf584958d502891
Diego Felipe Weber <diegofelipeweber@hotmail.com>
2026-03-26 14:55:57 -0300

    feat(dashboard) implementa renderizacao de vacinas no dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,6 +6,8 @@ const usuarioLogadoName = document.querySelector(".profile-inf__name");
 
 isUserLogged();
 
+
+
 /* EVENT LISTENER */
 if(logoutButton){
     logoutButton.addEventListener("click", () =>{
@@ -19,7 +21,62 @@ if(logoutButtonLink){
     });
 }
 
-/* FUNÇÕES */
+
+/* FUNÇÕES DASHBOARD */
+function renderUser(){
+    usuarioLogadoName.innerText = usuarioLogado.userName;
+}
+
+function renderVaccines(){
+    const vaccineContainer = document.getElementById("vaccines-container");
+
+    if(usuarioLogado.vaccines.length >= 1){
+        vaccineContainer.innerHTML = "";
+        vaccineContainer.classList.remove("vaccines-empty");
+        vaccineContainer.classList.add("vaccines-grid");
+        
+        usuarioLogado.vaccines.forEach(vaccine =>{
+            const cardHTML =
+            `<article id="vaccines-container" class="vaccines__card__container">
+                <div class="vaccines__card">
+                    <div class="vaccines__card-img">
+                        <img class="vaccines__card-img-img" src="../assets/images/vacina.png" alt="icone de vacina">
+                    </div>
+                    <div class="vaccines__card-text">
+                        <div class="vaccines_card-text__name">
+                            <h3 class="vaccines__card-text__title">${vaccine.vaccineName}</h3>
+                            <span class="status-badge pending">Aplicada</span>
+                        </div>
+                        
+                        <div class="vaccines__card-text__info">
+                            <p class="vaccines__card-text__info__dose">${vaccine.vaccineDose}</p>
+                            <p class="vaccines_card-text__info__date">${vaccine.vaccineDate}</p>
+                            <p class="vaccines__card-text__info__lote">${vaccine.vaccineLote}</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="vaccine__buttons">
+                    <button class="vaccine__buttons__edit" aria-label="Editar vacina"><img src="../assets/images/edit.png" alt="icone de editar"></button>
+                    <button class="vaccine__buttons__delete" aria-label="Excluir vacina"><img src="../assets/images/delete.png" alt="icone de excluir"></button>
+                </div>
+            </article>`
+
+            vaccineContainer.innerHTML += cardHTML;
+       })} else {
+            console.log("Opa, ta vazio")
+            const emptyCardHTML = 
+            `<article id='empty-slot' class='sections'>
+                <h2>Sem vacinas cadastradas ainda</h2>
+            </article>`;
+            vaccineContainer.innerHTML = emptyCardHTML;
+       }
+    
+}
+
+renderVaccines(); 
+
+/* OUTRAS FUNÇÕES */
 function logout(){
     const confirmLogout = confirm("Voce realmente deseja sair?");
 
@@ -30,17 +87,12 @@ function logout(){
     
 }
 
-function completeDashboardUser(){
-    console.log(usuarioLogado.userName)
-    usuarioLogadoName.innerText = usuarioLogado.userName;
+function emptyContainer(){
 
 }
-
-
-
 function isUserLogged(){
     if(usuarioLogado && usuarioLogadoName){
-        completeDashboardUser();
+        renderUser();
     } else if (!usuarioLogado){
         linkToLogin();
     }   
